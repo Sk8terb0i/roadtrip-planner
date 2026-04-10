cbb44d3dc17754bd469f5eed65d6200ca2e6e55f
sk8terboi <noe.arnold@outlook.com>
2026-04-10 20:53:07 +0200

    router

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import LandingPage from "./pages/LandingPage";
 import TripPlanner from "./pages/TripPlanner";
 
 function App() {
   return (
-    <Router basename="/roadtrip-planner">
+    // Note: With HashRouter, you usually remove the 'basename' prop
+    // because the hash handles the pathing relative to the root.
+    <Router>
       <Routes>
         <Route path="/" element={<LandingPage />} />
         <Route path="/trip/:tripId" element={<TripPlanner />} />
